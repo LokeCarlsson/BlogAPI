1b7c6a0503f454413c9ad5f10097666ac28d1e35
LokeCarlsson <lc222ak@student.lnu.se>
2017-02-14 13:44:22 +0100

    Added mongoose

--- src/db.js
+++ src/db.js
@@ -1,4 +1,14 @@
+import mongoose from 'mongoose'
+
+const db = 'mongodb://localhost/test'
+
+mongoose.connect(db)
+
+mongoose.connection.on('connected', function () {
+	console.log('Mongoose default connection open to ', db)
+})
+
 export default callback => {
 	// connect to a database if needed, then pass it to `callback`:
-	callback();
+	callback()
 }
