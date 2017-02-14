669cc8f98bd5e0c5c4895dfaa0404842c10110fe
LokeCarlsson <lc222ak@student.lnu.se>
2017-02-14 12:51:18 +0100

    Added boilerplate

---
+++ src/api/index.js
@@ -0,0 +1,15 @@
+import { version } from '../../package.json'
+import { Router } from 'express'
+import facets from './facets'
+
+export default ({ config, db }) => {
+	let api = Router()
+
+	api.use('/facets', facets({ config, db }))
+
+	api.get('/', (req, res) => {
+		res.json({ version })
+	})
+
+	return api
+}
