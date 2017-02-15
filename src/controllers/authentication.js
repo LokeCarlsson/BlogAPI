6acfa0183b979c3790d014b4d281c814faee747c
LokeCarlsson <lc222ak@student.lnu.se>
2017-02-15 21:39:19 +0100

    Added registration

--- src/controllers/authentication.js
+++ src/controllers/authentication.js
@@ -19,6 +19,48 @@ function setUserInfo(request) {
   }
 }
 
+function register (req, res, next) {
+  const email = req.body.email;
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const password = req.body.password;
+
+  if (!email)
+  return res.status(422).send({ error: 'You must provide an email address.'})
+
+  if (!firstName || !lastName)
+  return res.status(422).send({ error: 'You must provide your first and last name.'})
+
+  if (!password)
+  return res.status(422).send({ error: 'You must provide a password.' })
+
+  User.findOne({ email: email }, function(err, existingUser) {
+    if (err)
+    return next(err)
+
+    if (existingUser)
+    return res.status(422).send({ error: 'The email address is already taken!' })
+
+    let user = new User({
+      email: email,
+      password: password,
+      profile: { firstName: firstName, lastName: lastName }
+    })
+
+    user.save(function(err, user) {
+      if (err)
+      return next(err)
+
+      let userInfo = setUserInfo(user)
+
+      res.status(201).json({
+        token: 'JWT ' + generateToken(userInfo),
+        user: userInfo
+      })
+    })
+  })
+}
+
 function login (req, res, next) {
   const userInfo = setUserInfo(req.user)
 
@@ -28,6 +70,23 @@ function login (req, res, next) {
   })
 }
 
+function roleAuthorization (role) {
+  return function(req, res, next) {
+    const user = req.user
 
+    User.findById(user._id, function(err, foundUser) {
+      if (err) {
+        res.status(422).json({ error: 'No user was found.' })
+        return next(err)
+      }
+
+      if (foundUser.role == role)
+      return next()
+
+      res.status(401).json({ error: 'You are not authorized to view this content.' })
+      return next('Unauthorized')
+    })
+  }
+}
 
 export { login }
