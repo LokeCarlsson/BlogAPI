a07a9bf95749c9801794e475c024f95c0f1ae313
LokeCarlsson <lc222ak@student.lnu.se>
2017-02-15 17:51:52 +0100

    Added router file

---
+++ src/router.js
@@ -0,0 +1,33 @@
+import { login, register} from './controllers/authentication'
+import express from 'express'
+import passportService from './config/passport'
+import passport from 'passport'
+
+const requireAuth = passport.authenticate('jwt', { session: false })
+const requireLogin = passport.authenticate('local', { session: false })
+
+const REQUIRE_ADMIN = "Admin"
+const REQUIRE_OWNER = "Owner"
+const REQUIRE_CLIENT = "Client"
+const REQUIRE_MEMBER = "Member"
+
+
+
+function router (app) {
+  const router = express.Router()
+
+  router.post('/register', register)
+  router.get('/register', (req, res, next) => {
+    res.status(200).send('Registration!')
+  })
+
+  router.post('/login', requireLogin, login)
+  router.get('/login', (req, res, next) => {
+    res.status(200).send('Login!')
+  })
+
+  app.use('/', router)
+}
+
+
+export default router
