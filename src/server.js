c3ac3224cd70031c122216c3df91dae23882338d
LokeCarlsson <lc222ak@student.lnu.se>
2017-02-21 15:38:22 +0100

    Added some header access controls

--- src/server.js
+++ src/server.js
@@ -25,6 +25,14 @@ app.use(cors({
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
 router(app)
 
 app.server.listen(config.port || 3000, function () {
