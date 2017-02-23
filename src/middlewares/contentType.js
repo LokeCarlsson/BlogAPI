4c55eb3488d79963c40ceafd7f73c5401a7d0f97
LokeCarlsson <lc222ak@student.lnu.se>
2017-02-23 12:20:42 +0100

    Added a response error text

--- src/middlewares/contentType.js
+++ src/middlewares/contentType.js
@@ -1,6 +1,6 @@
 export default (req, res, next) => {
 	const contype = req.headers['content-type']
 	if (!contype || contype.indexOf('application/json') !== 0)
-	return res.send(400)
+	return res.status(400).send('The only content-type that is allowed is application/json')
 	next()
 }
